4e97ef3025b459e78ffe6f0655d5bdf922ccb561
Yurii <juracherniak@gmail.com>
2022-02-10 15:50:36 +0200

    author

--- models/post.js
+++ models/post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
     title: { type: String, required: true },
     text: { type: String, required: true },
-    author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    author: { type: String, required: true},
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
     published: { type: Boolean , default: false },
     date: { type: Date },
